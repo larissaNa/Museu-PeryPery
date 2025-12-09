1784bfe7b27d263c876f44cf5d10807101c3b9ca
Larissa Nascimento <120027246+larissaNa@users.noreply.github.com>
2025-12-09 17:05:34 -0300

    Update firebaseService.ts

--- src/infra/firebaseService.ts
+++ src/infra/firebaseService.ts
@@ -5,12 +5,12 @@ import { getStorage } from 'firebase/storage';
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyC52D7ZXXGhJxmqFR5Fzs2gKUSg6QPmGd8",
-  authDomain: "museu-perypery.firebaseapp.com",
-  projectId: "museu-perypery",
-  storageBucket: "museu-perypery.firebasestorage.app",
-  messagingSenderId: "968580407557",
-  appId: "1:968580407557:web:611e17839b5ab74e89176c"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
